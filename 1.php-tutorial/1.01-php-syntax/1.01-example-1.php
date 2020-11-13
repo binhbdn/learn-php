39679b93014b32a673101b856ec64abc3b0e94b3
binhbdn <binh.buidn@gmail.com>
2020-11-13 21:13:01 +0700

    Basic PHP Syntax

---
+++ 1.php-tutorial/1.01-php-syntax/1.01-example-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Syntax - Basic PHP Syntax</title>
+</head>
+<body>
+    <h1>My first PHP page</h1>
+    <h2>Cú pháp PHP cơ bản</h2>
+    <p>Một tập lệnh PHP có thể được đặt ở bất kỳ đâu trong tài liệu.</p>
+    <p>Một tập lệnh PHP bắt đầu bằng &lt;?phpvà kết thúc bằng ?>:</p>
+    <div>
+        &lt;?php
+        // PHP code goes here
+        ?>
+    </div>
+    <p>Phần mở rộng tệp mặc định cho tệp PHP là " .php".</p>
+    <p>Một tệp PHP thường chứa các thẻ HTML và một số mã kịch bản PHP.</p>
+    <p>File này là ví dụ về một tệp PHP đơn giản,có sử dụng hàm PHP tích hợp sẵn "echo" để xuất ra văn bản "Hello World!":</p>
+    <?php
+        echo "Hello World!<br>";
+    ?>
+    <p>Lưu ý: Các câu lệnh PHP kết thúc bằng dấu chấm phẩy ( ;).</p>
+</body>
+</html>
