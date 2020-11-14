af14ff3d45aed163b9aacb277b89d5a1dd28ce29
binhbdn <binh.buidn@gmail.com>
2020-11-14 11:00:32 +0700

    PHP echo and print statements

---
+++ 1.php-tutorial/1.04-php-echo-and-print/1.04-example-1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP echo and print Statements</title>
+    <style>
+        .red {
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Câu lệnh <span class="red">echo</span> và <span class="red">print</span> trong PHP</h1>
+    <p>Với PHP, có hai cách cơ bản để lấy đầu ra: <span class="red">echo</span> và <span class="red">print</span>.</p>
+    <p>Trong hướng dẫn này, chúng tôi sử dụng <span class="red">echo</span> hoặc <span class="red">print</span> trong hầu hết các ví dụ. Vì vậy, chương này chứa thêm một chút thông tin về hai câu lệnh đầu ra đó.</p>
+    <h2>Câu lệnh <span class="red">echo</span> và <span class="red">print</span> trong PHP</h2>
+    <p><span class="red">echo</span> và <span class="red">print</span> ít nhiều giống nhau. Cả hai đều được sử dụng để xuất dữ liệu ra màn hình.</p>
+    <p>Sự khác biệt nhỏ:</p>
+    <p><span class="red">echo</span> không có giá trị trả về trong khi <span class="red">print</span> có giá trị trả về là 1 nên nó có thể được sử dụng trong các biểu thức.</p>
+    <p><span class="red">echo</span> có thể nhận nhiều parameters / tham số (mặc dù việc sử dụng như vậy là hiếm) trong khi <span class="red">print</span> có thể lấy một argument / đối số.</p>
+    <p><span class="red">echo</span> nhanh hơn một chút so với <span class="red">print</span></p>
+</body>
+
+</html>
