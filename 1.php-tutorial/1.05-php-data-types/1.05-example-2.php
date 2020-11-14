ed4732ecaf4796de91c11990125f59b8cfd29bb9
binhbdn <binh.buidn@gmail.com>
2020-11-14 11:19:14 +0700

    PHP String

---
+++ 1.php-tutorial/1.05-php-data-types/1.05-example-2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP String</title>
+</head>
+
+<body>
+    <h1>PHP String / Chuỗi PHP</h1>
+    <p>Chuỗi là một chuỗi các ký tự, chẳng hạn như "Hello world!".</p>
+    <p>Một chuỗi có thể là bất kỳ văn bản nào bên trong dấu ngoặc kép. Bạn có thể sử dụng dấu ngoặc kép hoặc đơn:</p>
+    <p>Thí dụ</p>
+    <?php
+        $x = "Hello world!";
+        $y = 'Hello world!';
+
+        echo $x;
+        echo "<br>";
+        echo $y;
+    ?>
+</body>
+
+</html>
