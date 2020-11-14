0589ee6d7658257de15d2b578bd708962ba435ba
binhbdn <binh.buidn@gmail.com>
2020-11-14 14:33:55 +0700

    PHP Resource

---
+++ 1.php-tutorial/1.05-php-data-types/1.05-example-9.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Resource</title>
+    <style>.red {color:red;}</style>
+</head>
+
+<body>
+    <h1>PHP Resource / Tài nguyên PHP</h1>
+    <p>Kiểu tài nguyên đặc biệt không phải là kiểu dữ liệu thực tế. Nó là việc lưu trữ một tham chiếu đến các hàm và tài nguyên bên ngoài PHP.</p>
+    <p>Một ví dụ phổ biến của việc sử dụng kiểu dữ liệu tài nguyên là một cuộc gọi cơ sở dữ liệu.</p>
+    <p>Chúng tôi sẽ không nói về loại tài nguyên ở đây, vì nó là một chủ đề nâng cao.</p>
+</body>
+
+</html>
