96c453b1dc737babd6acdf758006bf72554ad6b7
binhbdn <binh.buidn@gmail.com>
2020-11-14 11:33:38 +0700

    PHP Float

---
+++ 1.php-tutorial/1.05-php-data-types/1.05-example-4.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Float</title>
+    <style>.red {color:red;}</style>
+</head>
+
+<body>
+    <h1>PHP Float / Số dấu phẩy động PHP</h1>
+    <p>Một số trôi nổi (số dấu phẩy động) là một số có dấu thập phân hoặc một số ở dạng số mũ.</p>
+    <p>Trong ví dụ sau, <span class="red">$x</span> là một số dấu phẩy động. Hàm <span class="red">var_dump()</span> trong PHP trả về kiểu dữ liệu và giá trị:</p>
+    <p>Thí dụ</p>
+    <?php
+        $x = 1981.;
+        var_dump($x);
+    ?>
+</body>
+
+</html>
