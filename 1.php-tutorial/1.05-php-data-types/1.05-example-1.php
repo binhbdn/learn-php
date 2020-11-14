f8aebcc1d6de7e07d839a0bda53ea0e2abc2727c
binhbdn <binh.buidn@gmail.com>
2020-11-14 11:16:07 +0700

    PHP Data Types

---
+++ 1.php-tutorial/1.05-php-data-types/1.05-example-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Data Types</title>
+</head>
+
+<body>
+    <h1>PHP Data Types / Các kiểu dữ liệu PHP</h1>
+    <p>Các biến có thể lưu trữ dữ liệu thuộc các kiểu khác nhau và các kiểu dữ liệu khác nhau có thể làm những việc khác nhau.</p>
+    <p>PHP hỗ trợ các kiểu dữ liệu sau:</p>
+    <ul>
+        <li>String</li>
+        <li>Integer</li>
+        <li>Float (floating point numbers - also called double / số dấu phẩy động - còn được gọi là kép)</li>
+        <li>Boolean</li>
+        <li>Array</li>
+        <li>Object</li>
+        <li>NULL / tạm dịch: VÔ GIÁ TRỊ</li>
+        <li>Resource / tạm dịch: Nguồn</li>
+    </ul>
+</body>
+
+</html>
