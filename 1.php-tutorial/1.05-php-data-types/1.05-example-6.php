2d3262ba8dcc9a6854a1aeba42c6f460bdb9604f
binhbdn <binh.buidn@gmail.com>
2020-11-14 12:01:48 +0700

    PHP Array

---
+++ 1.php-tutorial/1.05-php-data-types/1.05-example-6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Array</title>
+    <style>.red {color:red;}</style>
+</head>
+
+<body>
+    <h1>PHP Array / Mảng PHP</h1>
+    <p>Một mảng lưu trữ nhiều giá trị trong một biến duy nhất.</p>
+    <p>Trong ví dụ sau, <span class="red">$cars</span> là một mảng. Hàm <span class="red">var_dump()</span> trong PHP trả về kiểu dữ liệu và giá trị:</p>
+    <p>Thí dụ</p>
+    <?php
+        $cars = array("Volvo","BMW","Toyota");
+        var_dump($cars);
+    ?>
+    <p>Bạn sẽ tìm hiểu thêm rất nhiều về mảng trong các chương sau của hướng dẫn này.</p>
+</body>
+
+</html>
