6040ae2352e79a9e9d61de7b28fac3539dfd69da
binhbdn <binh.buidn@gmail.com>
2020-11-15 20:20:39 +0700

    PHP Superglobal

---
+++ 1.php-tutorial/1.24-php-superglobals/1.24-example-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Global Variables - Superglobals</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>PHP Global Variables - Superglobals / Các biến toàn cầu trong PHP - Superglobals</h1>
+    <p>Một số biến được xác định trước trong PHP là "superglobals", có nghĩa là chúng luôn có thể truy cập được, bất kể phạm vi - và bạn có thể truy cập chúng từ bất kỳ hàm, lớp hoặc tệp nào mà không cần phải làm bất kỳ điều gì đặc biệt.</p>
+    <p>Các biến superglobal trong PHP là:</p>
+    <ul>
+        <li><span class="red">$GLOBALS</span></li>
+        <li><span class="red">$_SERVER</span></li>
+        <li><span class="red">$_REQUEST</span></li>
+        <li><span class="red">$_POST</span></li>
+        <li><span class="red">$_GET</span></li>
+        <li><span class="red">$_FILES</span></li>
+        <li><span class="red">$_ENV</span></li>
+        <li><span class="red">$_COOKIE</span></li>
+        <li><span class="red">$_SESSION</span></li>
+    </ul>
+</body>
+</html>
