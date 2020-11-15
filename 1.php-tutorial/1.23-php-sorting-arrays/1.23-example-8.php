ae4f6d77d0f17151d7f02cf6d3bd286490fb5d38
binhbdn <binh.buidn@gmail.com>
2020-11-15 18:28:12 +0700

    ksort() - example 1

---
+++ 1.php-tutorial/1.23-php-sorting-arrays/1.23-example-8.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Sorting Arrays - ksort() - Example 1</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>PHP Sorting Arrays - Sắp xếp mảng PHP</h1>
+    <h2><span class="red">ksort()</span> - sắp xếp các mảng kết hợp theo thứ tự tăng dần, theo khóa</h2>
+    <p>Thí dụ 1:</p>
+    <?php
+    $age = array("Peter"=>"37", "Ben"=>"35", "Joe"=>"43");
+    echo "Before sorting:<br>";
+    foreach($age as $x => $x_value) {
+        echo "Key=" . $x . ", Value=" . $x_value;
+        echo "<br>";
+    }
+    ksort($age);
+    echo "After sorting:<br>";
+    foreach($age as $x => $x_value) {
+        echo "Key=" . $x . ", Value=" . $x_value;
+        echo "<br>";
+    }
+    ?>
+</body>
+</html>
