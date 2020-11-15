e3242c74fbd61c2b450c0cfc8478adf9324a5ac4
binhbdn <binh.buidn@gmail.com>
2020-11-15 14:33:54 +0700

    PHP do while Loop

---
+++ 1.php-tutorial/1.14-php-do-while-loop/1.14-example-1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP do while Loop</title>
+</head>
+<body>
+    <?php
+        $x = 6;
+        do {
+            echo "The number is: $x <br>";
+            $x++;
+        } while ($x <= 5);
+    ?>
+    <?php
+        $x = 1;
+
+        do {
+            echo "The number is: $x <br>";
+            $x++;
+        } while ($x <= 5);
+    ?>
+
+</body>
+</html>
