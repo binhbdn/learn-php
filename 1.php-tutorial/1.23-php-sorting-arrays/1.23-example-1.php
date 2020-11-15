d0d4d8bc2104530a6f6f1b7c14e40447cb7167aa
binhbdn <binh.buidn@gmail.com>
2020-11-15 18:04:20 +0700

    PHP - Sort Functions For Arrays

---
+++ 1.php-tutorial/1.23-php-sorting-arrays/1.23-example-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Sorting Arrays</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>PHP Sorting Arrays - Sắp xếp mảng PHP</h1>
+    <p>Các phần tử trong một mảng có thể được sắp xếp theo thứ tự bảng chữ cái hoặc số, giảm dần hoặc tăng dần.</p>
+    <h2>PHP - Sort Functions For Arrays / PHP - Các hàm sắp xếp cho mảng:</h2>
+    <ul>
+        <li><span class="red">sort()</span> - sắp xếp các mảng theo thứ tự tăng dần</li>
+        <li><span class="red">rsort()</span> - sắp xếp các mảng theo thứ tự giảm dần</li>
+        <li><span class="red">asort()</span> - sắp xếp các mảng kết hợp theo thứ tự tăng dần, theo giá trị</li>
+        <li><span class="red">arsort()</span> - sắp xếp các mảng kết hợp theo thứ tự giảm dần, theo giá trị</li>
+        <li><span class="red">ksort()</span> - sắp xếp các mảng kết hợp theo thứ tự tăng dần, theo khóa</li>
+        <li><span class="red">krsort()</span> - sắp xếp các mảng kết hợp theo thứ tự giảm dần, theo khóa</li>
+    </ul>
+</body>
+</html>
