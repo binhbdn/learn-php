59bacb5607e5d148ecffe2b79d4b73b37a861936
binhbdn <binh.buidn@gmail.com>
2020-12-09 21:04:21 +0700

    PHP Forms Handling - $_GET

---
+++ 2.php-forms/2.01-php-form-handling/2.01-example-2-welcome-get.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$_GET</title>
+</head>
+<body>
+    Welcome <?php echo $_GET["name"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
+</body>
+</html>
---
+++ 2.php-forms/2.01-php-form-handling/2.01-example-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Forms Handling - $_GET</title>
+</head>
+<body>
+    <form action="2.01-example-2-welcome-get.php" method="get">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <p>Đoạn mã trên khá đơn giản. Tuy nhiên, điều quan trọng nhất còn thiếu. Bạn cần xác thực dữ liệu biểu mẫu để bảo vệ tập lệnh của mình khỏi mã độc.</p>
+</body>
+</html>
