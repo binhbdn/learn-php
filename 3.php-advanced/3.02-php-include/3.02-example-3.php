59dc4942417491530d6c42b6078dc18880e89902
binhbdn <binh.buidn@gmail.com>
2020-12-12 18:02:35 +0700

    PHP Include Files

---
+++ 3.php-advanced/3.02-php-include/3.02-example-3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Include Files</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>PHP Include Files</h1>
+
+    <div class="menu">
+        <?php include 'menu.php';?>
+    </div>
+
+    <h2 class="red">Welcome to my home page!</h2>
+    <p>Some text.</p>
+    <p>Some more text.</p>
+    <?php include 'footer.php';?>
+</body>
+</html>
---
+++ 3.php-advanced/3.02-php-include/3.02-example-5.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Include Files</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>PHP Include Files</h1>
+
+    <div class="menu">
+        <?php include 'menu.php';?>
+    </div>
+
+    <h2 class="red">Welcome to my home page!</h2>
+    <p>
+        <?php include 'noFileExists.php';
+            echo "I have a $color $car.";
+        ?>
+    </p>
+    <p>Some more text.</p>
+    <?php include 'footer.php';?>
+</body>
+</html>
