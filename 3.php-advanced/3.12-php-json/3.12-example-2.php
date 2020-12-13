58a83f219f3bad7faecc9d5204e72c9de954f697
binhbdn <binh.buidn@gmail.com>
2020-12-13 20:45:55 +0700

    PHP - json_encode()

---
+++ 3.php-advanced/3.12-php-json/3.12-example-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - json_encode()</title>
+</head>
+<body>
+    <h1>PHP - json_encode()</h1>
+    <p>Hàm json_encode() được sử dụng để mã hóa một giá trị sang định dạng JSON.</p>
+    <p>Ví dụ sau cho thấy cách mã hóa một mảng kết hợp thành một đối tượng JSON:</p>
+    <?php
+        $cars = array("Volvo", "BMW", "Toyota");
+        echo json_encode($cars);
+    ?>
+</body>
+</html>
