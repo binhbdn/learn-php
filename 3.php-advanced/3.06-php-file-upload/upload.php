22d9792006cb91d6d997472104378be3fe988444
binhbdn <binh.buidn@gmail.com>
2020-12-13 10:32:17 +0700

    Complete Upload File PHP Script

--- 3.php-advanced/3.06-php-file-upload/upload.php
+++ 3.php-advanced/3.06-php-file-upload/upload.php
@@ -29,4 +29,15 @@ if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpe
   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
   $uploadOk = 0;
 }
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
 ?>
