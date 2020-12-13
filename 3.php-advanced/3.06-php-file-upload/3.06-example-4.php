d61bcea19734285197fe8d24df1a49c663307417
binhbdn <binh.buidn@gmail.com>
2020-12-13 10:19:49 +0700

    Limit File Size

---
+++ 3.php-advanced/3.06-php-file-upload/3.06-example-4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Limit File Size</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>Limit File Size</h1>
+    <p>Trường nhập tệp trong biểu mẫu HTML của chúng ta ở trên có tên là "fileToUpload".</p>
+    <p>Bây giờ, chúng ta muốn kiểm tra kích thước của tệp. Nếu tệp lớn hơn 500KB, thông báo lỗi sẽ hiển thị và $uploadOk được đặt thành 0</p>
+</body>
+</html>
--- 3.php-advanced/3.06-php-file-upload/upload.php
+++ 3.php-advanced/3.06-php-file-upload/upload.php
@@ -19,4 +19,9 @@ if (file_exists($target_file)) {
   echo "Sorry, file already exists.";
   $uploadOk = 0;
 }
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
 ?>
