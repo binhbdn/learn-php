fd43b2a2eb44d594fe602c590a45cf6656f5a589
binhbdn <binh.buidn@gmail.com>
2020-12-13 10:28:04 +0700

    Limit File Type

---
+++ 3.php-advanced/3.06-php-file-upload/3.06-example-5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Limit File Type</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1>Limit File Type</h1>
+    <p>Chúng ta chỉ cho phép người dùng tải lên các tệp JPG, JPEG, PNG và GIF. Tất cả các loại tệp khác đều đưa ra thông báo lỗi trước khi đặt <span class="red">$uploadOk</span> thành 0.</p>
+</body>
+</html>
--- 3.php-advanced/3.06-php-file-upload/upload.php
+++ 3.php-advanced/3.06-php-file-upload/upload.php
@@ -24,4 +24,9 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
   echo "Sorry, your file is too large.";
   $uploadOk = 0;
 }
+// Allow certain file formats
+if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
 ?>
