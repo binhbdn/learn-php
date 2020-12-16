59f6e5f115007bdec26420a6ef1c78082ab4050d
binhbdn <binh.buidn@gmail.com>
2020-12-16 09:05:40 +0700

    PHP OOP - Namespace Alias

---
+++ 4.php-oop/4.12-php-namespaces/4.12-example-5.php
@@ -0,0 +1,29 @@
+<?php
+    include "Html.php";
+    use Html as H;
+
+    $table = new H\Table();
+    $table->title = "My table";
+    $table->numRows = 5;
+
+    $row = new H\Row();
+    $row->numCells = 3;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP - Namespaces</title>
+    <style>.red{color:red;}</style>
+</head>
+<body>
+    <h1 class="red">PHP OOP - Namespaces / PHP OOP - Không gian tên</h1>
+    <h2 class="red">PHP OOP - Namespace Alias / Bí danh không gian tên</h2>
+    <p>It can be useful to give a namespace or class an alias to make it easier to write. This is done with the use keyword:</p>
+    <p class="red">Có thể hữu ích khi gán cho không gian tên hoặc lớp một bí danh nào đó cho để việc viết mã dễ dàng hơn. Điều này được thực hiện với từ khóa use:</p>
+    <h2 class="red">Ví dụ 5 (Đặt bí danh cho không gian tên:)</h2>
+    <?php $table->message(); ?>
+    <?php $row->message(); ?>
+</body>
+</html>
