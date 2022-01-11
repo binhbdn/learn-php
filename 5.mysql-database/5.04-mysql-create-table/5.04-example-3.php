dccb61c7f2a0e281f22fb64246ebee83a40de2b5
binhbdn <binh.buidn@gmail.com>
2022-01-11 14:23:49 +0700

    PDO

---
+++ 5.mysql-database/5.04-mysql-create-table/5.04-example-3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PDO</title>
+</head>
+
+<body>
+  <h1>Example (PDO)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDBPDO";
+
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      // sql to create table
+      $sql = "CREATE TABLE MyGuests (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      firstname VARCHAR(30) NOT NULL,
+      lastname VARCHAR(30) NOT NULL,
+      email VARCHAR(50),
+      reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      )";
+
+      // use exec() because no results are returned
+      $conn->exec($sql);
+      echo "Table MyGuests created successfully";
+    } catch(PDOException $e) {
+      echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+  ?>
+</body>
+
+</html>
