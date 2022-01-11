6bf043f26d3e8831e37b03de245861f2964a9b5e
binhbdn <binh.buidn@gmail.com>
2022-01-11 14:32:08 +0700

    PDO

---
+++ 5.mysql-database/5.05-mysql-insert-data/5.05-example-3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PDO</title>
+</head>
+
+<body>
+  <h1>Example (PDO)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDBPDO";
+
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+      VALUES ('John', 'Doe', 'john@example.com')";
+      // use exec() because no results are returned
+      $conn->exec($sql);
+      echo "New record created successfully";
+    } catch(PDOException $e) {
+      echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+  ?>
+</body>
+
+</html>
