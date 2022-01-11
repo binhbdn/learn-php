fcd171e595c83781cb247249372fcdb62a34b389
binhbdn <binh.buidn@gmail.com>
2022-01-11 16:18:01 +0700

    PDO + Prepared Statements

---
+++ 5.mysql-database/5.13-mysql-update-data/5.13-example-3.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PDO + Prepared Statements</title>
+</head>
+
+<body>
+  <h1>Example (PDO + Prepared Statements)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDBPDO";
+
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
+
+      // Prepare statement
+      $stmt = $conn->prepare($sql);
+
+      // execute the query
+      $stmt->execute();
+
+      // echo a message to say the UPDATE succeeded
+      echo $stmt->rowCount() . " records UPDATED successfully";
+    } catch(PDOException $e) {
+      echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+  ?>
+</body>
+
+</html>
