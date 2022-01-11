c549b00fce18cf14928faf6cd3f3d129ec410255
binhbdn <binh.buidn@gmail.com>
2022-01-11 14:38:27 +0700

    MySQLi Procedural

---
+++ 5.mysql-database/5.06-mysql-get-last-id/5.06-example-2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MySQLi Procedural</title>
+</head>
+
+<body>
+  <h1>Example (MySQLi Procedural)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDB";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+
+    if (mysqli_query($conn, $sql)) {
+      $last_id = mysqli_insert_id($conn);
+      echo "New record created successfully. Last inserted ID is: " . $last_id;
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+  ?>
+</body>
+
+</html>
