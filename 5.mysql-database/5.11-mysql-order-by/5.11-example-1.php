83d2269a53409b9d846bc1b4ee4fd90449125e30
binhbdn <binh.buidn@gmail.com>
2022-01-11 15:58:36 +0700

    MySQLi Object-Oriented

---
+++ 5.mysql-database/5.11-mysql-order-by/5.11-example-1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MySQLi Object-Oriented</title>
+</head>
+
+<body>
+  <h1>Example (MySQLi Object-Oriented)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, firstname, lastname FROM MyGuests ORDER BY lastname";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
+  ?>
+</body>
+
+</html>
