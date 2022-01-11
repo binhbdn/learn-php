7f2c3fbad5721710f1e952720929c2b5b46f5e54
binhbdn <binh.buidn@gmail.com>
2022-01-11 11:29:22 +0700

    MySQLi Object-Oriented

---
+++ 5.mysql-database/5.03-mysql-create-database/5.03-example-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MySQLi Object-Oriented</title>
+</head>
+
+<body>
+  <h1>Example (MySQLi Object-Oriented)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Create database
+    $sql = "CREATE DATABASE myDB";
+    if ($conn->query($sql) === TRUE) {
+      echo "Database created successfully";
+    } else {
+      echo "Error creating database: " . $conn->error;
+    }
+
+    $conn->close();
+  ?>
+</body>
+
+</html>
