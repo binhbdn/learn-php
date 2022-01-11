24da5778ff7a8e6ad8a2f40ab57b1cbcfd3fd8a4
binhbdn <binh.buidn@gmail.com>
2022-01-11 14:53:01 +0700

    MySQLi Procedural

---
+++ 5.mysql-database/5.07-mysql-insert-multiple/5.07-example-2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MySQLi Procedural</title>
+</head>
+
+<body>
+  <h1>Example (MySQLi Procedural)</h1>
+  <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "myDB";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com');";
+    $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('Mary', 'Moe', 'mary@example.com');";
+    $sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('Julie', 'Dooley', 'julie@example.com')";
+
+    if (mysqli_multi_query($conn, $sql)) {
+      echo "New records created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+  ?>
+</body>
+
+</html>
