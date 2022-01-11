794441b3cb18101a0bd0f811fc681c8472ccfa93
binhbdn <binh.buidn@gmail.com>
2022-01-11 10:54:46 +0700

    PHP MySQL Database

---
+++ 5.mysql-database/5.01-mysql-database/5.01-example-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP MySQL Database</title>
+</head>
+
+<body>
+  <h1>What is MySQL?</h1>
+  <ul>
+    <li>MySQL is a database system used on the web</li>
+    <li>MySQL is a database system that runs on a server</li>
+    <li>MySQL is ideal for both small and large applications</li>
+    <li>MySQL is very fast, reliable, and easy to use</li>
+    <li>MySQL uses standard SQL</li>
+    <li>MySQL compiles on a number of platforms</li>
+    <li>MySQL is free to download and use</li>
+    <li>MySQL is developed, distributed, and supported by Oracle Corporation</li>
+    <li>MySQL is named after co-founder Monty Widenius's daughter: My</li>
+  </ul>
+</body>
+
+</html>
